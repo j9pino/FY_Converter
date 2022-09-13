cbe0d981c8be3daee885f6ac96acc89ef25a12f1
Tyler Martindale <68250419+martindalete@users.noreply.github.com>
2022-09-13 08:19:08 -0400

    Update FY_converter.py

--- FY_converter.py
+++ FY_converter.py
@@ -2,22 +2,17 @@ import streamlit as st
 import pandas as pd
 import requests, json
 import time
+import base64
 
 st.experimental_memo.clear()
 st.set_page_config(page_title="FY Converter")
-st.title("Pubs: Calendar Year to Fiscal Year")
-bacon = ''
-#Scopus API headers
-headers = {'X-ELS-APIKey': st.secrets['API_KEY'], 
-           'Accept': 'application/json'}
+st.title("Pubs: Convert Calendar Year to Fiscal Year")
 
-#Scopus API query 
-url = 'https://api.elsevier.com/content/abstract/doi/'
+headers = {'Mailto':'martindalete@ornl.gov'}
 
 #create empty lists to which we will append API-gathered data
 results_list = []
-csv_thing = None
-   
+  
 #convert dataframe to csv for exporting purposes
 @st.experimental_memo(suppress_st_warning=True)
 def convert_df_to_csv(df):
@@ -27,70 +22,97 @@ def convert_df_to_csv(df):
 #main function that uses list of DOIs with API call
 @st.experimental_memo(suppress_st_warning=True)
 def api_loop(dataframe):
-    global csv_thing
     global dates_df
     for i in range(len(df)):
         percent_complete = (i+1)/len(df)
-        DOI = str(df.iloc[i]['DOIs'])
-        queryURL = url + DOI
-        r = requests.get(queryURL, headers=headers)
+        try:
+            DOI = str(df.iloc[i]['DOI'].replace(' ',''))
+        except:
+            DOI = ''
+            title = ''
+            pub_date = ''
+            FY = ''
+            results_list.append([DOI,title,pub_date,FY])
+            my_bar.progress(percent_complete)
+            continue
+        r = requests.get('https://api.crossref.org/works/'+DOI+'?mailto=martindalete@ornl.gov')        
         rText = r.text
         rJSON = json.loads(rText)
         try:
-            coverDate = rJSON['abstracts-retrieval-response']['coredata']['prism:coverDate']
+            title = rJSON['message']['title'][0]
         except:
-            coverDate = 'No Date Found'
+            title = 'No Article Title Found'
         try:
-            times_cited = rJSON['abstracts-retrieval-response']['coredata']['citedby-count']
+            try:
+                year = rJSON['message']['published']['date-parts'][0][0]
+            except:
+                year = 'XXXX'
+            try:
+                month = rJSON['message']['published']['date-parts'][0][1]
+            except:
+                month = 'XX'
+            try:
+                day = rJSON['message']['published']['date-parts'][0][2]
+            except:
+                day = 'XX'
+            pub_date = str(year)+'-'+ \
+                        str(month)+'-'+ \
+                        str(day)
         except:
-            times_cited = 'No Citations Found'
+            pub_date = ''
         try:
-            if int(coverDate[5:7]) >= 10:
-                year = int(coverDate[:4])+1
+            if month == 'XX':
+                FY = 'NA'
+            elif int(month) >= 10:
+                FY = int(year)+1
             else:
-                year = int(coverDate[:4])
-        except ValueError:
-            year = 'No Date Found'
-        results_list.append([DOI,coverDate,times_cited,year])
+                FY = year
+        except:
+            FY = 'No published date found'
+        results_list.append([DOI,title,pub_date,FY])
         my_bar.progress(percent_complete)
         time.sleep(0.05)
-    dates_df = pd.DataFrame(results_list, columns = ['DOI','coverDate','Times Cited', 'FY'])
+    dates_df = pd.DataFrame(results_list, columns = ['DOI','title','pub_date', 'FY'])
     
     dates_df = dates_df.reset_index(drop=True)
     dates_df['FY'] = dates_df['FY'].astype(str)
-    dates_df['coverDate'] = dates_df['coverDate'].astype(str)
+    dates_df['pub_date'] = dates_df['pub_date'].astype(str)
     
     #display final dataframe
     dates_df = dates_df.drop_duplicates()
     st.dataframe(dates_df)
-       
-    #convert df to csv
-    csv_thing = convert_df_to_csv(dates_df)
+    st.markdown(get_table_download_link(dates_df), unsafe_allow_html=True)
 
 @st.experimental_memo(suppress_st_warning=True)
-def show_download_button():
-    global csv_thing
-    st.download_button(
-        label="Download data as CSV",
-        data=csv_thing,
-        file_name='DOIs_with_FY.csv',
-        mime='text/csv')
-         
-#streamlit upload button
-data = st.file_uploader("Upload a CSV of DOIs, one per line, no header column",
-                       key = '1',
-                       help='Make sure your upload file is a CSV and only contains DOIs, one per line, with no header')
+def get_table_download_link(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    return f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'       
 
-#read in uploaded CSV and write to dataframe
-if data is not None:
-    df = pd.read_csv(data, header=None)
-    df = df.rename(columns={0: 'DOIs'})
-    #display dataframe of uploaded DOIs     
-    st.dataframe(df)
-    #introduce streamlit proress bar widget
-    my_bar = st.progress(0.0)
-    api_loop(df)
-    if csv_thing is not None:
-        st.balloons()              
-        st.success('Your Download is Ready!')
-        show_download_button()
+with st.form("my-form", clear_on_submit=True):
+    data = st.file_uploader('Upload data.  Make sure you have a column labeled "DOI". The standard RES output format is acceptable',
+                       key = '1',
+                       help='This widget accepts both CSV and XLSX files. The standard RES output format is acceptable.')
+    submitted = st.form_submit_button("Start the Process")
+
+    if submitted and data is not None:
+        st.write("Your Data:")
+        if data.name.lower().endswith('.csv'):
+            df = pd.read_csv(data, header=[0])
+            #display dataframe of uploaded DOIs     
+            st.dataframe(df)
+            #introduce streamlit proress bar widget
+            my_bar = st.progress(0.0)
+            api_loop(df)
+            st.balloons()              
+            st.success('Your Download is Ready!')
+
+        elif data.name.lower().endswith('.xlsx'):
+            df = pd.read_excel(data, header=[0])
+            #display dataframe of uploaded DOIs     
+            st.dataframe(df)
+            #introduce streamlit proress bar widget
+            my_bar = st.progress(0.0)
+            api_loop(df)
+            st.balloons()              
+            st.success('Your Download is Ready!')        
